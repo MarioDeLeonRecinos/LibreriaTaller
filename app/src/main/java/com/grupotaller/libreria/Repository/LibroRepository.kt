a18ea64fc8608f079766f2f8355c436617b6c824
MarioDeLeonRecinos <37392326+MarioDeLeonRecinos@users.noreply.github.com>
2019-05-23 14:59:36 -0600

    asi se manejarian los cambios en el librodao para el repository hacer cambios en los otros

--- app/src/main/java/com/grupotaller/libreria/Repository/LibroRepository.kt
+++ app/src/main/java/com/grupotaller/libreria/Repository/LibroRepository.kt
@@ -6,8 +6,9 @@ import com.grupotaller.libreria.DAO.LibroDAO
 import com.grupotaller.libreria.Entity.Libro
 
 class LibroRepository(val librodao: LibroDAO) {
+    
     val allLibros: LiveData<List<Libro>> = librodao.getAll();
-    val allfavorites: LiveData<List<Libro>> = librodao.getFavorito();
+    
     @WorkerThread
     suspend fun insert(libro: Libro){
         librodao.Insert(libro);
@@ -22,5 +23,14 @@ class LibroRepository(val librodao: LibroDAO) {
     suspend fun search(nombre: String): List<Libro> {
         return librodao.getSearch(nombre);
     }
-
+    
+    @WorkerThread
+    suspend fun getFavoritos(){
+        allLibros=librodao.getFavorito();
+    }
+    
+    @WorkerThread
+    suspend fun getAll(){
+        allLibros=librodao.getAll();
+    }
 }
