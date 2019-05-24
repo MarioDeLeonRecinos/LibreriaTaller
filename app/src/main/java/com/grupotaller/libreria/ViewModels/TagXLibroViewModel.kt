2fda73120f50bdc39adcfa67198aa8f998cd5945
RoxanaCortez <00502211@uca.edu.sv>
2019-05-23 22:12:53 -0600

    viewmodel de tablas cruzadas tagxlibro

---
+++ app/src/main/java/com/grupotaller/libreria/ViewModels/TagXLibroViewModel.kt
@@ -0,0 +1,34 @@
+package com.grupotaller.libreria.ViewModels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.grupotaller.libreria.Entity.Tag
+import com.grupotaller.libreria.Entity.TagXLibro
+import com.grupotaller.libreria.Repository.TagXLibroRepository
+import com.grupotaller.libreria.RoomDatabase.LibreriaRoomDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TagXLibroViewModel(app: Application): AndroidViewModel(app) {
+    private val repository: TagXLibroRepository;
+
+    init {
+        val tagsxlibrosdao = LibreriaRoomDatabase.getDatabase(app, viewModelScope);
+        repository = TagXLibroRepository(tagsxlibrosdao.TagXLibroDAO());
+    }
+
+    fun insert(txl: TagXLibro) = viewModelScope.launch(Dispatchers.IO)
+    {
+        repository.insert(txl);
+    }
+
+    suspend fun deleteAll(){
+        repository.deleteAll();
+    }
+
+    fun getTags(id:Int): LiveData<List<Tag>> {
+        return repository.getTags(id)
+    }
+}
