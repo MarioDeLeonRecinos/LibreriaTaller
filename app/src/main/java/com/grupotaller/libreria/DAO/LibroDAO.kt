8708692d00768499059fbfa193f7239344bee795
MarioDeLeonRecinos <00353715@uca.edu.sv>
2019-05-24 16:10:19 -0600

    añadido el metodo update de favoritos para la funcionalidad que se habia pedido

--- app/src/main/java/com/grupotaller/libreria/DAO/LibroDAO.kt
+++ app/src/main/java/com/grupotaller/libreria/DAO/LibroDAO.kt
@@ -21,6 +21,10 @@ interface LibroDAO {
     @Query("SELECT * FROM Libro where Titulo LIKE :nombre")
     fun getSearch(vararg nombre: String): LiveData<List<Libro>>
 
+    //Como Sqlite maneja booleanos con un int siendo 0 false y 1 true se va a alternar entre estos
+    @Query("UPDATE Libro SET Favorito=:Boolean WHERE IdL = :idL")
+    fun updateFav(idL: Int, Boolean: Int)
+
     @Query("DELETE FROM Libro")
     fun deleteAll()
 }
